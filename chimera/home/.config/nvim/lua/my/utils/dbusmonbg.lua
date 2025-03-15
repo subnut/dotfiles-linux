ccc4ed4918828deb1e4a9cde7c0e7855b2f40afb
Subhaditya Nath <sn03.general@gmail.com>
2025-03-15 23:02:43 +0530

    nvim(dbusmonbg): use vim.system instead of uv.spawn
    Using uv.spawn causes the dbus-monitor process to stay running even
    after neovim exits, which isn't what we intended. We want dbus-monitor
    to exit when neovim exits.

--- chimera/home/.config/nvim/lua/my/utils/dbusmonbg.lua
+++ chimera/home/.config/nvim/lua/my/utils/dbusmonbg.lua
@@ -3,12 +3,9 @@ if vim.fn.executable(cmd) ~= 1 then
   return  -- we need dbus-monitor
 end
 
-local M = {}
-local opts = {}
-local uv = vim.uv
-
-opts.args = {
-  "--binary", vim.iter{
+cmd = {
+  cmd, "--binary",
+  vim.iter{
     "type=signal",
     "path=/org/freedesktop/portal/desktop",
     "interface=org.freedesktop.portal.Settings",
@@ -18,28 +15,20 @@ opts.args = {
   }:join(","),
 }
 
-local stdout = uv.new_pipe()
-local stderr = uv.new_pipe()
-opts.stdio = { nil, stdout, stderr }
-
-local onexit = function(code, signal)
-  local str = "[EXIT] dbus-monitor exited (exit code: %d)"
-  local cb = require'my.utils.callback'
-  vim.schedule(cb(
-    vim.notify,
-    str:format(code),
-    vim.log.levels.INFO))
-end
+local opts = {
+    text = false,
+    clear_env = true,
+}
 
 opts.env = {}
-local env = uv.os_environ()
+local env = vim.uv.os_environ()
 for key,val in pairs(env) do
   if key:sub(1, 5) == "DBUS_" then
     table.insert(opts.env, key.."="..val)
   end
 end
 
-local stdout_handler = function (err, data)
+opts.stdout = function(err, data)
   assert(not err, err)
   if data == nil then
     return  -- EOF
@@ -60,12 +49,7 @@ local stdout_handler = function (err, data)
   end
 end
 
-M.handle, M.pid = uv.spawn(
-  cmd, opts, onexit
-)
-
-stdout:read_start(stdout_handler)
-stderr:read_start(function(err, data)
+opts.stderr = function(err, data)
   assert(not err, err)
   local str = "[STDERR] dbus-monitor: %s"
   local cb = require'my.utils.callback'
@@ -73,6 +57,15 @@ stderr:read_start(function(err, data)
     vim.notify,
     str:format(data),
     vim.log.levels.INFO))
-end)
+end
 
-return M
+local onexit = function(obj)
+  local str = "[EXIT] dbus-monitor exited (exit code: %d)"
+  local cb = require'my.utils.callback'
+  vim.schedule(cb(
+    vim.notify,
+    str:format(obj.code),
+    vim.log.levels.INFO))
+end
+
+return vim.system(cmd, opts, onexit)
